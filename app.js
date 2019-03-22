98e376143abaa9894c7fbf12f8c49f06c7938fcf
KerstingJ <Kersting.Josh@Gmail.com>
2019-03-22 17:09:18 -0400

    refactored code base

--- app.js
+++ app.js
@@ -22,67 +22,82 @@ const articles = document.querySelectorAll('.bio');
 
 
 members.forEach( member => {
-    member.addEventListener('click', () => {
-        let clicked = member;
+    member.addEventListener('click', (event) => {
+
+        // console.log(event)
+        // console.log(event.target)
+        // console.log(event.currentTarget)
+
+        // stop this event from going into the world
+        event.stopPropagation();
+
+        // if we clicked on an already active image, lets get reset and get out of this
+        if (event.currentTarget.classList.contains("clicked")) {
+            clickOff();
+            return
+        }
+
+        //remove .clicked from every member and bio
+        members.forEach(member => {
+            //get article that corresponds with member
+            let bioID = member.dataset.bio;
+            let bio = document.querySelector(`article[data-bio='${bioID}']`);
+
+            //remove clicked from member and article
+            member.classList.remove('clicked');
+            bio.classList.remove('clicked');
+
+            //tween out member
+            TweenMax.to(member, 1, {opacity: .2, scale: .5});
+        })
+
+        //get the clicked member and associated bio
+        let clickedMember = event.currentTarget;
+        let bioID = clickedMember.dataset.bio;
+        let clickedBio = document.querySelector(`article[data-bio='${bioID}']`);
+
+        //set member and bio to clicked
+        clickedMember.classList.add("clicked");
+        clickedBio.classList.add("clicked");
+
+        // tween our active member and bio
+        TweenMax.to(clickedMember, 1, {scale: 1.3, opacity: 1});
+        TweenMax.from(clickedBio, 1, {opacity: 0, delay:1, clearProps:"all"});
         
-        let bioID = member.dataset.bio;
-        let bioArticle = document.querySelector(`article[data-bio='${bioID}']`);
-        
-        articles.forEach( article => {
-            if (article === bioArticle) {
-                article.classList.add('clicked');
-            } else {
-                article.classList.remove('clicked');
-            }
-        } );
-            
-            TweenMax.to(member, 1, {scale: 1.3});
-            TweenMax.from(bioArticle, 2, {opacity: 0, delay:1});
-        
-            if (bioID === 'amir')  {
-                TweenMax.to(bioArticle, 1, {y: -400});
-            }
-        
-            if (bioID === 'cynthia') {
-                TweenMax.to(bioArticle, 1, {y: -400});
-            }
-        
-            if (bioID === 'will') {
-                TweenMax.to(bioArticle, 1, {y: -400, x: 300});
-            }
-        
-            if (bioID === 'hargo') {
-                TweenMax.to(bioArticle, 1, {y: -400, x: -300});
-            }          
-        
-        members.forEach( member => {
-            if (member !== clicked) {
-                TweenMax.to(member, 1, {opacity: .2, scale: .5});
-                member.classList.remove('clicked');
-                
-            } else {
-                TweenMax.to(member, 1, {opacity: 1});
-                member.classList.add('clicked');
-            }
-        });
-        
-        console.log(clicked);
-        clicked.addEventListener('click',() => {
-            clicked.classList.remove('clicked');
-            bioArticle.classList.remove('clicked');
-            TweenMax.to(clicked, 1, {scale:1});
-        });
+        // set bio to appropriate position
+        if (bioID === 'amir')  {
+            TweenMax.to(clickedBio, 1, {y: -400});
+        }
+    
+        if (bioID === 'cynthia') {
+            TweenMax.to(clickedBio, 1, {y: -400});
+        }
+    
+        if (bioID === 'will') {
+            TweenMax.to(clickedBio, 1, {y: -400, x: 300});
+        }
+    
+        if (bioID === 'hargo') {
+            TweenMax.to(clickedBio, 1, {y: -400, x: -300});
+        }
+
+        //add event listener to reset 
+        window.addEventListener('click', clickOff);
         
     });
 });
 
+// reset us back to the original state of the site
+const clickOff = () => {
+    console.log("im really clicked off right now")
+    let clickedMembers = document.querySelectorAll(".member.clicked");
+    let clickedBios = document.querySelectorAll(".bio.clicked");
 
+    clickedMembers.forEach(mem => { mem.classList.remove('clicked') });
+    clickedBios.forEach(bio => bio.classList.remove('clicked'));
 
+    //set all members back to og size
+    document.querySelectorAll('.member').forEach(member => TweenMax.to(member, 1, {scale:1, opacity: 1}));
 
-
-
-
-
-
-
-
+    window.removeEventListener('click', clickOff);
+}
